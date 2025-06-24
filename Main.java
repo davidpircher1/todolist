f60772945d315a1f50cb916e324c8c80a00c35ac
davidpircher1 <davidalexander.pircher@gmail.com>
2025-06-24 23:32:05 +0200

    Main

---
+++ Main.java
@@ -0,0 +1,7 @@
+import Frames.FrameFactory;
+public class Main {
+    public static void main(String[] args) {
+        FrameFactory frameFactory = new FrameFactory();
+        frameFactory.create("main");
+    }
+}
